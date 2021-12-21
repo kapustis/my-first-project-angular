81ff52e82aa14d62433707d50b339832899e0b28
Gennady <kapustis73@gmail.com>
2021-12-21 16:57:23 +0200

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+export interface Card{
+  title:string
+  body:string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'my-first-project';
+  showCart = true;
+
+  cards: Card[] = [
+    {title: "Card #1",body: "#1 some text "},
+    {title: "Card #2",body: "#2 some text "},
+    {title: "Card #3",body: "#3 some text "},
+    {title: "Card #4",body: "#4 some text "},
+  ]
+
+  showCard(){
+    this.showCart = !this.showCart;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CartComponent} from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
